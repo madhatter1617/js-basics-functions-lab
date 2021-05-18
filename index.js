79832cb105b6696b2a439b924a10a66747b63ee4
Rosa <rosarenee@att.net>
2021-05-17 23:28:15 -0500

    got stuck not done

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks){
+
+if (blocks >= 42 ){
+    let homeSpot = 42;
+    let first = blocks - homeSpot;
+ return first   
+}else if ( blcoks < 42 ){
+
+    let homeSpot = 42;
+    let first = homeSpot - blocks;
+return first 
+}}
